904d7ccf39faaae13fea8e52888bd117c711486c
Ozhan Akdag <45542447+Ozhana@users.noreply.github.com>
2026-03-09 15:48:15 +0300

    Create t1_data_code.py

---
+++ GREP_Marathon/task_1_Error_analysis_lof_file/t1_data_code.py
@@ -0,0 +1,12 @@
+import random
+
+log_levels = ["INFO", "DEBUG", "ERROR", "CRITICAL", "WARNING", "error", "Critical"]
+messages = ["Connection failed", "User logged in", "Disk full", "Service started", "Timeout occurred"]
+
+with open("system.log", "w") as f:
+    for i in range(100):
+        level = random.choice(log_levels)
+        msg = random.choice(messages)
+        f.write(f"2026-03-06 14:{random.randint(10,59)} - {level} - {msg}\n")
+
+print("system.log dosyası oluşturuldu.")
