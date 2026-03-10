b7a3230921cea7c2e839c7d500d88b708839d03a
Ozhan Akdag <45542447+Ozhana@users.noreply.github.com>
2026-03-10 07:11:08 +0300

    Add files via upload

---
+++ GREP_Marathon/T14_PID Controller Log Analysis/g14_PID_controller.py
@@ -0,0 +1,15 @@
+import random
+
+with open("g14_pid_debug.log", "w") as f:
+    f.write("Time,Target,Current,Error\n")
+    for i in range(300):
+        target = 500
+        # Normalde hata küçük, ama sonda robot pistten çıkıyor
+        if i > 250:
+            current = target + random.randint(50, 100)
+        else:
+            current = target + random.randint(-5, 5)
+        error = current - target
+        f.write(f"10:00:{i:03d},{target},{current},{error}\n")
+
+print("g14_pid_debug.log oluşturuldu.")
